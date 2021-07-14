05c91fb74275ca42b8900d7b25894632b92b850d
Danielle Kavanagh <danikav1989@gmail.com>
2021-07-14 04:11:10 +0100

    js tests completed

--- index.js
+++ index.js
@@ -10,37 +10,60 @@ const data = require("./data");
  * POSITION: Midfielder
  * PLAYER 2...
  */
+const players = data.getPlayers()
 
- // Your code
+function getData() {
+  players.forEach((player, index) => {
+    console.log(`PLAYER ${index + 1}\nNAME: ${player.name}\nLASTNAME: ${player.lastname}\nPOSITION: ${player.position.charAt(0).toUpperCase() + player.position.slice(1)}`)
+  });
+}
 
-
+// getData()
 
 /**
  * Test 2
  * Write a function to log in the console an array with only the names of the players, ordered A-Z
  */
+function sortNames(){
+const names = []
+for (let player of players){
+  names.push(player.name)
+};
+names.sort()
+console.log(names)
+};
 
-// Your code
-
-
-
+// sortNames();
 
 
 /**
  * Test 3
  * Write a function to log in the console the average number of goals there will be in a match if all the players in the data play on it
  * scoringChance means how many goals per 100 matches the player will score
- * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals 
+ * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals
  * Output example -> Goals per match: 2.61
  */
+function getAverageGoals() {
+let totalScoringChance = 0
+for (player of players){
+  totalScoringChance += parseInt(player.scoringChance)
+}
+totalScoringChance = totalScoringChance/100
+console.log(`Goals per match:`, totalScoringChance)
+}
 
-// Your code
-
-
+// getAverageGoals()
 
 /**
  * Test 4
  * Write a function that accepts a name, and logs the position of the player with that name
  */
-
-// Your code
+function findByName(searchname){
+var found = players.find(function(player, index) {
+ return player.name.toLowerCase() === searchname.toLowerCase()
+});
+console.log(found.position)
+}
+// findByName("lucas")
+// findByName("Erik")
+// findByName("LucAs")
